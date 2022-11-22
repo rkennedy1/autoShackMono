9e5c05c8048f06e95bd52c3f65b1b282893dcb36
rkennedy1 <rskennedy99@gmail.com>
2022-11-21 22:09:04 -0800

    default to loading gif (#41)
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>

--- shackServer/index.js
+++ shackServer/index.js
@@ -9,22 +9,22 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let lastPicture = ""
+let lastPicture = "loading_gif.gif";
 
 app.listen(process.env.PORT, () =>
   console.log("API is running on port ", process.env.PORT)
 );
 
-app.use(express.static('public')); 
-app.use('/images', express.static('images'));
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
 
-app.post('/lastPicture', function (req, res) {
-    lastPicture = req.body.fileName
-    res.send('a ok')
-})
-app.get('/lastPicture', function (req, res) {
-    res.json({lastPic: lastPicture})
-})
+app.post("/lastPicture", function (req, res) {
+  lastPicture = req.body.fileName;
+  res.send("a ok");
+});
+app.get("/lastPicture", function (req, res) {
+  res.json({ lastPic: lastPicture });
+});
 
 var dataRouter = require("./data.js");
 app.use("/data", dataRouter);
