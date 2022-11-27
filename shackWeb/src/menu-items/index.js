e6c6ce70c55ce11a2ff7784b13347221b42b0f79
Mike Kennedy <mskennedy67@gmail.com>
2022-11-27 12:19:51 -0800

    SHACK-10 <message>

--- shackWeb/src/menu-items/index.js
+++ shackWeb/src/menu-items/index.js
@@ -1,12 +1,11 @@
 // project import
 import pages from './pages';
 import dashboard from './dashboard';
-import utilities from './utilities';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-    items: [dashboard, pages, utilities]
+    items: [dashboard, pages]
 };
 
 export default menuItems;
