6e66b9201fd5e6be618642b07599d67c43387dac
Michael Kennedy <mskennedy67@gmail.com>
2022-12-17 15:05:05 -0800

    removed unused menu (#58)

--- shackWeb/src/pages/authentication/auth-forms/AuthLogin.js
+++ shackWeb/src/pages/authentication/auth-forms/AuthLogin.js
@@ -23,7 +23,6 @@ import * as Yup from 'yup';
 import { Formik } from 'formik';
 
 // project import
-import FirebaseSocial from './FirebaseSocial';
 import AnimateButton from 'components/@extended/AnimateButton';
 
 // assets
@@ -168,9 +167,6 @@ const AuthLogin = () => {
                                 <Divider>
                                     <Typography variant="caption"> Login with</Typography>
                                 </Divider>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FirebaseSocial />
                             </Grid>
                         </Grid>
                     </form>
