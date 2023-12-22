37f20ae598aa8550ade287ab0f51eee2f6da2532
rkennedy1 <rskennedy99@gmail.com>
2023-12-21 19:37:47 -0800

    SHACK-47: update DHT

--- shackController/scripts/humidityTempSensor.py
+++ shackController/scripts/humidityTempSensor.py
@@ -1,16 +1,13 @@
-#import board
-import adafruit_dht
+# import board
+import DHT
 import time
+import pigpio
 
 
-def handler(signum, frame):
-    raise Exception('Action took too much time')
-
-
-class HumidityTempSensor():
+class HumidityTempSensor:
     def __init__(self, pin, logger):
         self.pin = pin
-        self.sensor = adafruit_dht.DHT22(self.pin)
+        self.sensor = DHT.sensor(pigpio.pi(), pin, model=2)
         self.humidity, self.temperature = 0, 0
         self.logger = logger
 
@@ -18,17 +15,19 @@ class HumidityTempSensor():
         # signal.alarm(15) #Set the parameter to the amount of seconds you want to wait
 
     def getReading(self):
-        try:
-            # get temp/humidy readings and
-            temperature_c = self.sensor.temperature
-            self.temperature = temperature_c * (9 / 5) + 32
-            self.humidity = self.sensor.humidity
-
-        except RuntimeError as error:
-            # Errors happen fairly often, DHT's are hard to read, just keep going
-            self.logger.error(error.args[0])
-            time.sleep(2.0)
-            return
-        except Exception as error:
-            self.sensor.exit()
-            raise error
+        tries = 5
+        while tries:
+            try:
+                timestamp, gpio, status, temperature, humidity = self.sensor.read()
+                # get temp/humidy readings and
+                if status == DHT.DHT_TIMEOUT:  # no response from sensor
+                    print("NO RESPONSE FROM SENSOR")
+                    raise RuntimeError
+                if status == DHT.DHT_GOOD:
+                    print(timestamp + gpio)
+                    print("temp: " + temperature)
+                    print("humidity: " + humidity)
+                time.sleep(2)
+                tries -= 1
+            except KeyboardInterrupt:
+                break
