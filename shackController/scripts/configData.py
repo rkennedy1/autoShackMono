d41eaca9f031a707a8aa8882e40e34435484c4cb
rkennedy1 <rskennedy99@gmail.com>
2024-03-20 23:22:33 -0700

    Fix db connection errors (#97)
    * fix linting issues
    
    * fix db connection in configdata
    
    * fix mysql
    
    * fix no data error
    
    * fixed logging and added reconnect
    
    * fixed flowrate parm
    
    * fix unreachable else
    
    * remove excessive flow logging
    
    * fix unchanged bug
    
    * add connection = false
    
    * reconnected to db logger info
    
    * consolidate err msgs

--- shackController/scripts/configData.py
+++ shackController/scripts/configData.py
@@ -1,67 +1,143 @@
-import time
-import mysql.connector
 import os
-from dotenv import load_dotenv
-from datetime import datetime
+import time
 import json
+import logging
+from datetime import datetime
+from dotenv import load_dotenv
+import mysql.connector
+from sqlDatabase import Database
+from logging.handlers import TimedRotatingFileHandler
+
 load_dotenv()
 
 
 class ConfigData:
-    def __init__(self):
-        self.scheduleData = []
+    """
+    The `ConfigData` class is used to retrieve configuration data from a database or a JSON file.
 
-    def getConfigurationDataFromDB(self):
-        db = mysql.connector.connect(
-            host=os.getenv('MYSQL_HOST'),
-            user=os.getenv('MYSQL_USER'),
-            password=os.getenv('MYSQL_PASSWORD'),
-            database=os.getenv('MYSQL_DATABASE')
-        )
-        cursor = db.cursor()
-        self.desiredPumpStateOn = False
+    Attributes:
+        schedule_data (list): A list of dictionaries containing the configuration data.
+        desired_pump_state_on (bool): A boolean that indicates whether the pump should be on.
+        database (Database): An instance of the `Database` class.
+        logger (Logger): An instance of the `Logger` class.
 
-        cursor.execute('SELECT * FROM shackSchedule')
-        data = cursor.fetchall()
-        configurationData = []
-        # print(data)
+    Methods:
+        parse_configuration_data: Parses the configuration data and returns a list of dictionaries.
+        get_configuration_data_from_db: Retrieves configuration data from the database.
+        get_configuration_data_from_file: Retrieves the configuration data from a JSON file.
+        set_desired_pump_state: Sets the desired pump state to on based on a schedule defined in the `schedule_data`.
+    """
+
+    def __init__(self, database, logger):
+        self.schedule_data = []
+        self.desired_pump_state_on = False
+        self.database = database
+        self.logger = logger
+
+    def parse_configuration_data(self, data):
+        """
+        Parses the configuration data and returns a list of dictionaries.
+
+        Args:
+            data (list): The configuration data to be parsed.
+
+        Returns:
+            list: A list of dictionaries containing the parsed configuration data. Each dictionary
+            contains the following keys:
+                - id: The ID of the configuration.
+                - start_hour: The starting hour of the configuration (as an integer).
+                - duration: The duration of the configuration (as an integer).
+        """
+        parsed_config_data = []
         for line in data:
-            configurationData.append(
-                {'id': line[0], 'start_hour': int(line[1]), 'duration': int(line[2])})
-        self.scheduleData = configurationData
-        # print(self.scheduleData)
+            parsed_config_data.append(
+                {
+                    "id": line[0],
+                    "start_hour": int(line[1]),
+                    "duration": int(line[2]),
+                }
+            )
+        return parsed_config_data
 
-    def getConfigurationDataFromFile(self):
-        self.desiredPumpStateOn = False
-        configurationData = []
-        # get the current directory so that we can get the configuration file
-        with open(os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))+'/shack.config.json') as f:
-            configurationData = json.load(f)
-            f.close()
-        self.scheduleData = configurationData["events"]
+    def get_configuration_data_from_db(self):
+        """
+        Retrieves configuration data from the database.
 
-    def setDesiredPumpState(self):
-        for item in self.scheduleData:
-            # initialize start time with current time
-            curTime = datetime.now()
-            start_time = curTime
-            start_time = start_time.replace(
-                hour=item["start_hour"], minute=0)
-            # initialize end time with start time
-            duration = item["duration"]
-            end_time = start_time
-            end_time = end_time.replace(minute=duration)
-            if start_time <= curTime < end_time:
-                self.desiredPumpStateOn = True
+        Returns:
+            list: A list of dictionaries containing the configuration data.
+                  Each dictionary represents a row from the 'shackSchedule' table
+                  and contains the following keys:
+                  - 'id': The ID of the schedule entry.
+                  - 'start_hour': The starting hour of the schedule entry.
+                  - 'duration': The duration of the schedule entry.
+        """
+        self.desired_pump_state_on = False
+        query = "SELECT * FROM shackSchedule"
+        try:
+            data = self.database.query_data(query)
+            if data:
+                self.schedule_data = self.parse_configuration_data(data)
+        except mysql.connector.Error as e:
+            self.logger.error(f"Error retrieving configuration data from database: {e}")
+            self.get_configuration_data_from_file()
+
+    def get_configuration_data_from_file(self):
+        """
+        Retrieves the configuration data from a JSON file.
+
+        Returns:
+            list: The configuration data retrieved from the file.
+        """
+        self.desired_pump_state_on = False
+        configuration_data = []
+
+        config_file_path = os.path.join(
+            os.path.dirname(__file__), "..", "shack.config.json"
+        )
+
+        with open(config_file_path, encoding="utf-8") as file:
+            configuration_data = json.load(file)
+
+        self.schedule_data = configuration_data.get("events", [])
+
+    def set_desired_pump_state(self):
+        """
+        This function sets the desired pump state to on based on a schedule defined in the
+        `schedule_data`.
+        """
+        for item in self.schedule_data:
+            cur_time = datetime.now()
+            start_time = cur_time.replace(hour=item["start_hour"], minute=0)
+            end_time = start_time.replace(minute=item["duration"])
+            if start_time <= cur_time < end_time:
+                self.desired_pump_state_on = True
 
 
 if __name__ == "__main__":
-    s = time.perf_counter()
-    elapsed = time.perf_counter() - s
-    data = ConfigData()
-    data.getConfigurationDataFromDB()
-    data.setDesiredPumpState()
-    print(data.scheduleData)
-    print(data.desiredPumpStateOn)
+    s = time.perf_counter()  # Start timer
+
+    # Set up logging
+    root_dir = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+    log = logging.getLogger("ConfigData")
+    log.setLevel(logging.INFO)
+    handler = TimedRotatingFileHandler(
+        os.path.join(root_dir, "logs", "configdata.log"),
+        when="D",
+        interval=1,
+        backupCount=5,
+    )
+    handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
+    log.addHandler(handler)
+    log.info("Begin AutoShack")
+    db = Database()
+
+    config_data = ConfigData(db, log)
+    config_data.get_configuration_data_from_db()
+    config_data.set_desired_pump_state()
+
+    elapsed = time.perf_counter() - s  # End timer
+
+    print(config_data.schedule_data)
+    print(config_data.desired_pump_state_on)
     print(f"{__file__} executed in {elapsed:0.2f} seconds.")
     print("End AutoShack")
--- shackController/scripts/flow.py
+++ shackController/scripts/flow.py
@@ -1,8 +1,25 @@
-import RPi.GPIO as GPIO
 import time
+import RPi.GPIO as GPIO
 
 
 class FlowSensor:
+    """
+    The FlowSensor class represents a flow sensor that measures the flow rate of a liquid.
+    It provides methods to count pulses and calculate the flow rate based on the count value.
+
+    Attributes:
+        pin: The GPIO pin number to which the flow sensor is connected.
+        logger: The logger object for logging messages.
+        count: The number of pulses counted by the sensor.
+        start_counter: A boolean value indicating whether the counter is active.
+        flow: The flow rate calculated based on the count value.
+
+    Methods:
+        count_pulse: Increments the count and calculates flow based on the count.
+        setup: Configures a GPIO pin for input with a pull-up resistor and adds an event detection.
+        get_flow: Calculates the flow rate based on a count value and a time delay.
+    """
+
     def __init__(self, pin, logger):
         self.logger = logger
         self.pin = pin
@@ -11,20 +28,30 @@ class FlowSensor:
         self.flow = 0
         self.setup()
 
-    def countPulse(self, channel):
+    def count_pulse(self, channel):
+        """
+        The function `count_pulse` increments a count and calculates flow based on the count.
+        """
         if self.start_counter:
-            self.count = self.count+1
+            self.count = self.count + 1
             self.flow = self.count / (60 * 7.5)
 
     def setup(self):
+        """
+        The setup function configures a GPIO pin for input with a pull-up resistor and adds an event
+        detection for a falling edge trigger.
+        """
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=self.countPulse)
+        GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=self.count_pulse)
 
-    def getFlow(self):
+    def get_flow(self):
+        """
+        This function calculates the flow rate based on a count value and a time delay.
+        """
         self.start_counter = True
         time.sleep(1)
         self.start_counter = False
-        self.flow = (self.count * 60 * 2.25 / 1000)
+        self.flow = self.count * 60 * 2.25 / 1000
         self.count = 0
